1d6872d628f844f34905269abe3ff260f6cf4db6
JamesMutahi <mutahijames0@gmail.com>
2021-08-31 12:36:02 +0300

    make detail view url

--- web/urls.py
+++ web/urls.py
@@ -5,4 +5,6 @@ from web import views
 urlpatterns = [
     path('', views.landing_page, name='landing-page'),
     path('home/', views.home, name='home'),
+    # path('search/', views.post_search, name='route_search'),
+    path('route/<int:pk>/', views.RouteDetailView.as_view(), name='route-detail'),
 ]
